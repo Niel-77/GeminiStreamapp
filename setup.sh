c13dd81507d7ec19a9c4ec5b26b1fe417e909d15
Nirmal Prasad Panta <nil.panta77@gmail.com>
2024-06-17 22:08:21 +0545

    changes in setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Update package lists
-sudo apt-get update
-
-# Install FEniCS without recommended packages
-sudo apt-get install --no-install-recommends -y fenics
-
 # post_setup.sh
-curl -s http://fenicsproject.org/fenics-install.sh | bash
-
-
-# Verify FEniCS installation
-fenics-version
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:fenics-packages/fenics
+sudo apt-get update
+sudo apt-get install -y fenics
+sudo apt-get dist-upgrade -y
