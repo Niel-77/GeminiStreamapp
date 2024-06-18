ab5a8bc384a5ac44ad45b37a04f9ed5d64911abf
Niel-77 <67886596+Niel-77@users.noreply.github.com>
2024-06-18 05:22:55 +0000

    Removed packages and setup

--- setup.sh
+++
@@ -1,7 +0,0 @@
-# post_setup.sh
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:fenics-packages/fenics
-sudo apt-get update
-sudo apt-get install -y fenics
-sudo apt-get dist-upgrade -y
